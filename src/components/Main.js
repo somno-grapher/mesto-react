35fd63f23b5004b098106e5856151a4d874cf054
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-04-08 17:19:07 +0300

    feat: provide api query for user data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,50 @@
+import api from '../utils/api';
+import avatarPlaceholderPath from '../images/avatar.jpg';
+import React from 'react';
+
 function Main(props) {
+
+  const [userName, setUserName] = React.useState('Ваше имя...');
+  const [userDescription, setUserDescription] = React.useState('О вас...');
+  const [userAvatar, setUserAvatar] = React.useState(avatarPlaceholderPath);
+
+  React.useEffect(getInitialDataFromServer, []);
+
+  function getInitialDataFromServer() {
+    Promise.all([
+      api.getCurrentUser(),
+      // api.getInitialCards()
+    ])
+      .then(([
+        jsonResponseUser,
+        // jsonResponseCards
+      ]) => {
+        setUserName(jsonResponseUser.name);
+        setUserDescription(jsonResponseUser.about);
+        setUserAvatar(jsonResponseUser.avatar);
+        // currentUserId = jsonResponseUser._id;
+        // userInfo.setInitialUserInfo(jsonResponseUser);
+        // photoGridList.generateAndAddInitialItems(jsonResponseCards);
+      })
+      .catch(err => { console.log(err); });
+  }
 
   return (
     <main className="content page__content">
       <section className="profile content__profile">
         <button className="profile__avatar-container"
           onClick={props.handleEditAvatarClick}>
-          <img src="<%=require('./images/avatar.jpg')%>"
+          <img src={userAvatar}
             alt="Аватар"
             className="avatar" />
         </button>
         <div className="profile__info">
-          <h1 className="profile__name">Ваше имя...</h1>
+          <h1 className="profile__name">{userName}</h1>
           <button type="button"
             className="edit-button profile__edit-button edit-button_type_profile"
             onClick={props.handleEditProfileClick}>
           </button>
-          <p className="profile__about">О вас...</p>
+          <p className="profile__about">{userDescription}</p>
         </div>
         <button type="button"
           className="add-button profile__add-button add-button_type_card"
