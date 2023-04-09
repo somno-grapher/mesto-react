21d9a9f0e4b5cac7662c33111d99b3225278187c
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-04-09 18:42:13 +0300

    refactor: destructurize PopupWithForm props

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,19 +1,25 @@
-function PopupWithForm(props) {
-  const stateClass = props.isOpen && 'popup_opened';
+function PopupWithForm({
+  buttonText,
+  children,
+  isOpen,
+  name,
+  onClose,
+  title }) {
+  const stateClass = isOpen && 'popup_opened';
   return (
-    <div className={`popup popup_type_${props.name} ${stateClass}`}>
+    <div className={`popup popup_type_${name} ${stateClass}`}>
       <div className="popup__container popup__container_type_edit-window">
         <button type="button"
           className="close-button popup__close-button"
-          onClick={props.onClose}>
+          onClick={onClose}>
         </button>
-        <h2 className="popup__title popup__title_type_edit-window">{props.title}</h2>
-        <form name={`${props.name}-form`}
+        <h2 className="popup__title popup__title_type_edit-window">{title}</h2>
+        <form name={`${name}-form`}
           className="popup__form"
           noValidate>
-          {props.children}
+          {children}
           <button type="submit"
-            className="save-button popup__save-button">{props.buttonText}</button>
+            className="save-button popup__save-button">{buttonText}</button>
         </form>
       </div>
     </div>
