12d7493dc4a1f736851874e60ec385ad53db7b92
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-04-09 18:39:26 +0300

    refactor: destructurize Card props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,9 @@
-function Card(props) {
+function Card({
+  card,
+  onCardClick }) {
 
   function handleCardClick() {
-    props.onCardClick(props.card);
+    onCardClick(card);
   }
 
   return (
@@ -10,16 +12,16 @@ function Card(props) {
         className="delete-button card__delete-button"></button>
       <img
         className="card__photo"
-        src={props.card.link}
-        alt={props.card.name}
+        src={card.link}
+        alt={card.name}
         onClick={handleCardClick}
       />
       <div className="card__info">
-        <h2 className="card__title">{props.card.name}</h2>
+        <h2 className="card__title">{card.name}</h2>
         <div className="card__info-likes">
           <button type="button"
             className="like-button card__like-button"></button>
-          <p className="card__counter-likes">{props.card.likes.length}</p>
+          <p className="card__counter-likes">{card.likes.length}</p>
         </div>
       </div>
     </li>
