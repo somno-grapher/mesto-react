27e8f17203474f9e98f4b00f1ddaf5466757c7e1
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-04-09 15:29:43 +0300

    refactor: make some improvements

--- src/components/App.js
+++ src/components/App.js
@@ -131,15 +131,6 @@ function App() {
         onClose={closeAllPopups}
       />
 
-      {/* <div className="popup popup_type_show-photo">
-        <figure className="popup__container popup__container_type_photo">
-          <button type="button"
-            className="close-button popup__close-button"></button>
-          <img className="full-photo" />
-          <figcaption className="popup__title popup__title_type_photo"></figcaption>
-        </figure>
-      </div> */}
-
       <div className="popup popup_type_confirm">
         <div className="popup__container popup__container_type_edit-window">
           <button type="button"
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,5 +1,5 @@
 function PopupWithForm(props) {
-  const stateClass = props.isOpen ? 'popup_opened' : '';
+  const stateClass = props.isOpen && 'popup_opened';
   return (
     <div className={`popup popup_type_${props.name} ${stateClass}`}>
       <div className="popup__container popup__container_type_edit-window">
@@ -7,7 +7,6 @@ function PopupWithForm(props) {
           className="close-button popup__close-button"
           onClick={props.onClose}>
         </button>
-        {/* ! props for h2 class confirmation popup needed */}
         <h2 className="popup__title popup__title_type_edit-window">{props.title}</h2>
         <form name={`${props.name}-form`}
           className="popup__form"
