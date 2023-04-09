6ed1051e346a8a2daead71e2417acefc58f9c643
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-04-09 18:44:14 +0300

    refactor: destructurize ImagePopup props

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,25 +1,28 @@
-function ImagePopup(props) {
-  const stateClass = props.card.link && 'popup_opened';
+function ImagePopup({
+  card,
+  onClose }) {
+  const stateClass = card.link && 'popup_opened';
   return (
     <div className={`popup popup_type_show-photo ${stateClass}`} >
       <figure className="popup__container popup__container_type_photo">
         <button
           type="button"
           className="close-button popup__close-button"
-          onClick={props.onClose}
+          onClick={onClose}
         >
         </button>
         <img
           className="full-photo"
-          src={props.card.link}
-          alt={props.card.name}
+          src={card.link}
+          alt={card.name}
         />
         <figcaption className="popup__title popup__title_type_photo">
-          {props.card.name}
+          {card.name}
         </figcaption>
       </figure>
     </div>
   );
 }
+
 
 export default ImagePopup;
